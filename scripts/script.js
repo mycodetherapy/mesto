2a76ae0c9ef08b3c7c80be9d7ceb68c6dc0444dc
Max Ovchinnikov <max7ton@yandex.ru>
2021-09-14 21:44:11 +0300

    change function editProfile & formSubmitHandler

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,46 @@
 let profileInfo = document.querySelector('.profile__info');
 let popup = document.querySelector('.popup');
 let edit = profileInfo.querySelector('.profile__button');
-let closedForm = popup.querySelector('.form__closed');
+let closedForm = popup.querySelector('.popup__close');
 
 let profileTitleText = (profileInfo.querySelector('.profile__title')).textContent;
 let profileSubtitleText = (profileInfo.querySelector('.profile__subtitle')).textContent;
-let formInputName = popup.querySelector('.form__input_name');
-let formInputProfession = popup.querySelector('.form__input_profession');
+let formInputName = popup.querySelector('.form__input_type_name');
+let formInputProfession = popup.querySelector('.form__input_type_profession');
 
-let profileTitle = profileInfo.querySelector('.profile__title');
-let buttonSave = popup.querySelector('.form__button');
+let formElement = popup.querySelector('.form');
 
-let elementContainer = document.querySelector('.elements__grid-container');
-
+//Функция editProfile при открытии формы выводит в полях ввода значения из профиля.
+//В функции editProfile временно используется return, чтобы 
+//значения полей ввода не сбрасывлись после повторного вызова функции.
 function editProfile() {
     popup.classList.add('popup_opened');
-    formInputName.value = profileTitleText;
-    formInputProfession.value = profileSubtitleText;
+    if(formInputName.value === "" || formInputName.value === profileTitleText){
+        formInputName.value = profileTitleText;
+        formInputProfession.value = profileSubtitleText;
+    }
+    else {return;}
 }
 
-function exitProfile() {
-    popup.classList.remove('popup_opened');
-}
-
-function saveProfile() {
-    profileInfo.innerHTML = `
-        <p class="profile__title">${formInputName.value}</p>
-        <button class="profile__button" onclick="editProfile()"></button>
-        <p class="profile__subtitle">${formInputProfession.value}</p>
-    `
+//Функция formSubmitHandler при сохранении формы меняет текст в профиле.
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    let name = formInputName.value;
+    let profession = formInputProfession.value;
+    profileInfo.querySelector('.profile__title').textContent = name;
+    profileInfo.querySelector('.profile__subtitle').textContent = profession;
+    
     exitProfile();
 }
 
-function like(event) {
-    let target = event.target;
-    if (target.className != 'element__like') return;
-    target.classList.toggle('element__like_active');
+//Функция exitProfile закрывает окно формы.
+function exitProfile() { 
+    popup.classList.remove('popup_opened');
 }
 
-
-edit.addEventListener('click', editProfile);
-closedForm.addEventListener('click', exitProfile);
-buttonSave.addEventListener('click', saveProfile);
-elementContainer.addEventListener('click', like);
-
+edit.addEventListener('click', editProfile);  //Ловим клик на кнопке редактирования профиля.
+closedForm.addEventListener('click', exitProfile); //Ловим клик на кнопке закрытия окна формы
+formElement.addEventListener('submit', formSubmitHandler); //Ловим событие сохранения формы.
 
 
 
