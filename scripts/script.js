fadf2b85bd1627864c75cde963d0e942b2f6f651
Max Ovchinnikov <max7ton@yandex.ru>
2021-10-12 16:35:41 +0300

    refactor: delete invalid variable & rename function

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,6 @@ const creatElementButton = document.querySelector(".profile__button-add");
 const popupProfile = document.querySelector(".popup_type_edit-profile");
 const popupCreatElement = document.querySelector(".popup_type_creat-element");
 const popupTypeImage = document.querySelector(".popup_type_image");
-//const closePopupProfile = popupProfile.querySelector(".popup__close");
-//const closePopupCreatElement = popupCreatElement.querySelector(".popup__close");
 const formInputName = popupProfile.querySelector(".form__input_type_name");
 const formInputProfession = popupProfile.querySelector(
   ".form__input_type_profession"
@@ -14,12 +12,9 @@ const profileTitle = profileInfo.querySelector(".profile__title");
 const profileSubtitle = profileInfo.querySelector(".profile__subtitle");
 const formElementEditProfile = popupProfile.querySelector(".form");
 const formElementCreatElement = popupCreatElement.querySelector(".form");
-const elementBox = document.querySelector(".elements__grid-container");
 const elementList = document.querySelector(".elements__grid-container");
 const elementTemplate = document.querySelector(".element-template");
 const popups = Array.from(document.querySelectorAll(".popup"));
-
-//const closePopupImage = popupTypeImage.querySelector(".popup__close");
 const popupImage = popupTypeImage.querySelector(".popup__element-image");
 const popupImageCaption = popupTypeImage.querySelector(".popup__image-caption");
 const inputPlace = popupCreatElement.querySelector(".form__input_type_place");
@@ -57,7 +52,7 @@ const initialCards = [
 ];
 
 //Сreates element.
-function createCard(item) {
+const createCard = (item) => {
   const newCard = elementTemplate.content.cloneNode(true);
   const namePlace = newCard.querySelector(".element__title");
   const linkPlace = newCard.querySelector(".element__image");
@@ -69,13 +64,13 @@ function createCard(item) {
 }
 
 //Adds elements from an array.
-function addCards(cards) {
+const addCards = (cards) => {
   const newCards = cards.map(createCard);
   elementList.append(...newCards);
 }
 
 //adds element.
-function addElement(event) {
+const addElement = (event) => {
   event.preventDefault();
   const newCard = createCard({
     name: inputPlace.value,
@@ -84,11 +79,10 @@ function addElement(event) {
   elementList.prepend(newCard);
   closePopup(popupCreatElement);
   cleanInput(inputPlace, inputPlaceLink);
-  //inactiveButton(formButtonSave);
 }
 
 //Saves the text from the input to the profile.
-function formSubmitHandler(event) {
+const formSubmitHandler = (event) => {
   event.preventDefault();
   profileTitle.textContent = formInputName.value; //name;
   profileSubtitle.textContent = formInputProfession.value; //profession;
@@ -96,7 +90,7 @@ function formSubmitHandler(event) {
 }
 
 //Tears off an image for viewing
-function viewImage(event) {
+const viewImage = (event) => {
   const target = event.target;
   const elementContent = target.parentElement;
   const targetImage = elementContent.querySelector(".element__image");
@@ -107,75 +101,43 @@ function viewImage(event) {
 
   openPopup(popupTypeImage);
   focusElement(popupTypeImage);
-  //keydownHandler(popupTypeImage);
 }
 
-//toggles popup.
-// function togglePopup(popup) {
-//   popup.classList.toggle("popup_opened");
-// }
-
-function inactiveButton(button) {
+//Makes the button inactive
+const inactiveButton = (button) => {
   button.classList.add("form__button-save_inactive");
 }
 
 //Outputs the text from the profile to the input.
-function fillInputText() {
+const fillInputText = () => {
   formInputName.value = profileTitle.textContent;
   formInputProfession.value = profileSubtitle.textContent;
 }
 
 //Cleans up inputs.
-function cleanInput(firstInput, secondInput) {
+const cleanInput = (firstInput, secondInput) => {
   firstInput.value = "";
   secondInput.value = "";
 }
 
 //Toggles likes.
-function switchLike(event) {
+const switchLike = (event) => {
   const target = event.target;
   target.classList.toggle("element__like_active");
 }
 
 //Removes an element.
-function removeElement(event) {
+const removeElement = (event) => {
   const listItemRemove = event.currentTarget.closest(".element");
   listItemRemove.remove();
 }
 
+//Set focus.
 const focusElement = (element) => {
   element.querySelector(".popup__container").focus();
 };
 
-//listens to events in the element.
-function setListenerToElement(element) {
-  element.querySelector(".element__like").addEventListener("click", switchLike);
-
-  element
-    .querySelector(".element__delete")
-    .addEventListener("click", removeElement);
-
-  element.querySelector(".element__image").addEventListener("click", viewImage);
-}
-
-//Catches a click on the edit button.
-editProfileButton.addEventListener("click", () => {
-  openPopup(popupProfile);
-  fillInputText();
-  focusElement(popupProfile);
-  inactiveButton(buttonSaveProfile);
-  resetInputError(popupProfile);
-});
-
-//Catches a click on the add element button.
-creatElementButton.addEventListener("click", () => {
-  openPopup(popupCreatElement);
-  cleanInput(inputPlace, inputPlaceLink);
-  focusElement(popupCreatElement);
-  inactiveButton(buttonSaveCard);
-  resetInputError(popupCreatElement);
-});
-
+//Close handler popup.
 const closeHandler = () => {
   popups.forEach((popup) => {
     popup.addEventListener("click", (evt) => {
@@ -189,37 +151,7 @@ const closeHandler = () => {
   });
 };
 
-//forms close handler.
-// const closeClickHandler = () => {
-//   popups.forEach((button) => {
-//     button.querySelector(".popup__close").addEventListener("click", (event) => {
-//       const targetForm = event.currentTarget.closest(".popup");
-//       togglePopup(targetForm);
-//       hideInputErrorIfClose(
-//         targetForm,
-//         Array.from(targetForm.querySelectorAll(".form__input"))
-//       );
-//     });
-//   });
-// };
-
-// //Click overlay handler.
-// const clickOverlayHandler = () => {
-//   popups.forEach((popup) => {
-//     popup
-//       .querySelector(".popup__container")
-//       .parentElement.addEventListener("click", (event) => {
-//         if (!event.target.classList.contains("popup")) return;
-//         const targetPopup = event.target;
-//         togglePopup(targetPopup);
-//         hideInputErrorIfClose(
-//           targetPopup,
-//           Array.from(targetPopup.querySelectorAll(".form__input"))
-//         );
-//       });
-//   });
-// };
-
+//Close handler popup by escape.
 const closeByEscape = (evt) => {
   if (evt.key === "Escape") {
     const openedPopup = document.querySelector(".popup_opened");
@@ -227,32 +159,20 @@ const closeByEscape = (evt) => {
   }
 };
 
+//Open popup.
 const openPopup = (popup) => {
   popup.classList.add("popup_opened");
   popup.addEventListener("keydown", closeByEscape);
 };
 
+//Close popup.
 const closePopup = (popup) => {
   popup.classList.remove("popup_opened");
   popup.removeEventListener("keydown", closeByEscape);
 };
 
-//Keydown esc handler.
-// const keydownHandler = (popup) => {
-//   popup.addEventListener("keydown", function escListener(event) {
-//     if (event.key === "Escape") {
-//       const targetPopup = event.target.closest(".popup_opened");
-//       togglePopup(targetPopup);
-//       hideInputErrorIfClose(
-//         targetPopup,
-//         Array.from(targetPopup.querySelectorAll(".form__input"))
-//       );
-//       popup.removeEventListener("keydown", escListener);
-//     }
-//   });
-// };
-
-const resetInputError = (popup) => {
+//Hide errors.
+const hideError = (popup) => {
   const inputElements = Array.from(popup.querySelectorAll(".form__input_type_error"));
   const errorElements = Array.from(popup.querySelectorAll(".form__input-error"));
   inputElements.forEach((element) => {
@@ -264,28 +184,44 @@ const resetInputError = (popup) => {
   });
 }
 
-//Reset error.
-// const hideInputErrorIfClose = (formElements, inputElements) => {
-//   const errorElements = Array.from(
-//     formElements.querySelectorAll(".form__input-error")
-//   );
-//   inputElements.forEach((element) => {
-//     element.classList.remove("form__input_type_error");
-//   });
-//   errorElements.forEach((element) => {
-//     element.classList.remove("form__input-error_active");
-//     element.textContent = "";
-//   });
-// };
+//listens to events in the element.
+const setListenerToElement = (element) => {
+  element.querySelector(".element__like").addEventListener("click", switchLike);
 
-//Catches the submission of the form for adding an element.
+  element
+    .querySelector(".element__delete")
+    .addEventListener("click", removeElement);
+
+  element.querySelector(".element__image").addEventListener("click", viewImage);
+}
+
+//Click handler edit profile button.
+editProfileButton.addEventListener("click", () => {
+  openPopup(popupProfile);
+  fillInputText();
+  focusElement(popupProfile);
+  inactiveButton(buttonSaveProfile);
+  hideError(popupProfile);
+});
+
+//Click handler add element button.
+creatElementButton.addEventListener("click", () => {
+  openPopup(popupCreatElement);
+  cleanInput(inputPlace, inputPlaceLink);
+  focusElement(popupCreatElement);
+  inactiveButton(buttonSaveCard);
+  hideError(popupCreatElement);
+});
+
+//Submit handler creat element.
 formElementCreatElement.addEventListener("submit", addElement);
-//Catches sending the profile editing form.
+
+//Submit handler edit profile.
 formElementEditProfile.addEventListener("submit", formSubmitHandler);
 
 //Displaying initial content.
 addCards(initialCards);
 
+//Start close handler popup.
 closeHandler();
-//closeClickHandler();
-//clickOverlayHandler();
+
